98b2a35add20823fa4df52f49427779bea1b249c
KiruthikaML <kiruthika@smartglossa.com>
2016-10-18 15:36:17 +0530

    add new module

---
+++ WebContent/js/target2.js
@@ -0,0 +1,32 @@
+function mouseover()
+	{
+       var x=document.getElementById('image');
+		x.style.opacity="2";
+	}
+function mouseout()
+{
+  var x=document.getElementById('image');
+		x.style.opacity="0";
+}
+function mouseover1()
+	{
+       var d=document.getElementById('image1');
+		d.style.opacity="2";
+	}
+function mouseout1()
+{
+  var d=document.getElementById('image1');
+		d.style.opacity="0";
+}
+
+function mouseover2()
+	{
+       var a=document.getElementById('image2');
+		a.style.opacity="2";
+	}
+function mouseout2()
+{
+  var a=document.getElementById('image2');
+		a.style.opacity="0";
+}
+
